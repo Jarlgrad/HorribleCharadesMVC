692c2ab81c81a0db67845b3bb0f70b7fe9414ef9
Tim <timphan92@gmail.com>
2016-07-25 21:14:05 +0200

    test

---
+++ src/HorribleCharadesMVC/Models/HCDB.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HorribleCharadesMVC.Models
+{
+    public class HCDB
+    {
+    }
+}
