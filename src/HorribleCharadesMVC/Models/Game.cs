994c6fd1e15a2d77c1ed6bdbdd2151e4a182ff10
Tim <timphan92@gmail.com>
2016-07-26 21:43:31 +0200

    Published to Azure

---
+++ src/HorribleCharadesMVC/Models/Game.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HorribleCharadesMVC.Models
+{
+    public class Game
+    {
+
+    }
+}
