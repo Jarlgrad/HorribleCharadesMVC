ccfc3f0195e3f38d89d1ced74e0c6170682fa01f
Toni Lee <contact.tonilee@gmail.com>
2016-07-28 22:27:04 +0200

    Lagt till en timer(Se trello) Skapat ett nytt lag när man trycker på new game -Toni

--- src/HorribleCharadesMVC/Models/Game.cs
+++ src/HorribleCharadesMVC/Models/Game.cs
@@ -10,13 +10,17 @@ namespace HorribleCharadesMVC.Models
         public Game()
         {
             GenerateCode();
+            Teams = new List<Player>();
+            Teams.Add(new Player { PlayerName = "Team1" });
+
         }
         public List<Player> Teams { get; set; }
         public string Code { get; private set; }
         public void GenerateCode()
+            //Genererar en kod som användare skriver in för att komma in på samma spel
         {
             string newCode = "";
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 5; i++) //Antal tecken
             {
                 if (RandomUtils.ReturnValue(2) == 0) //50-50 om det blir en siffra eller bokstav
                 {
